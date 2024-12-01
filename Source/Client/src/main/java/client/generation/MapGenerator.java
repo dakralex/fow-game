b8af402b1f4836fd95e39cc9cdf459f33479c8cd
Daniel Kral <a11908284@unet.univie.ac.at>
2024-12-01 16:49:01 +0100

    client: generation: implement simple random map generator
    Implements a random `MapGenerator`, which generates a pre-defined fort
    structure and generates a pseudorandom terrain distribution around the existing
    fort structure.
    
    To ensure that the required amount of map nodes per terrain are reached, the
    required terrain types are pre-queued, where the remaining possible terrains
    are filled with mountain terrain types to maximize mountains in the generated
    `GameMap`. Afterwards, the terrain queue is pseudo-randomized and used to
    generate the remaining `GameMapNode`s for the `GameMap`.
    
    Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>
    
    Date:      Sun Dec 1 17:08:01 2024 +0100

--- Source/Client/src/main/java/client/generation/MapGenerator.java
+++ Source/Client/src/main/java/client/generation/MapGenerator.java
@@ -3,14 +3,22 @@ package client.generation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import client.map.GameMap;
 import client.map.GameMapNode;
 import client.map.MapDirection;
 import client.map.Position;
+import client.map.TerrainType;
 import client.validation.HalfMapValidator;
 
 public class MapGenerator {
@@ -23,6 +31,10 @@ public class MapGenerator {
 
     private static final int X_FORT_BORDER_SIZE = 2;
     private static final int Y_FORT_BORDER_SIZE = 2;
+
+    private static final int GRASS_MIN_AMOUNT = 24;
+    private static final int MOUNTAIN_MIN_AMOUNT = 5;
+    private static final int WATER_MIN_AMOUNT = 7;
 
     private final Random random;
     private final FortGenerator fortGenerator;
@@ -39,13 +51,48 @@ public class MapGenerator {
         return new Position(x, y);
     }
 
+    private Set<Position> generatePositionRange() {
+        return IntStream.range(0, MAP_SIZE)
+                .mapToObj(index -> new Position(index % X_SIZE, Math.floorDiv(index, X_SIZE)))
+                .collect(Collectors.toSet());
+    }
+
+    private List<TerrainType> generateTerrainTypeQueue(int amount) {
+        List<TerrainType> terrainTypeQueue = new ArrayList<>(amount);
+
+        terrainTypeQueue.addAll(Collections.nCopies(GRASS_MIN_AMOUNT, TerrainType.GRASS));
+        terrainTypeQueue.addAll(Collections.nCopies(MOUNTAIN_MIN_AMOUNT, TerrainType.MOUNTAIN));
+        terrainTypeQueue.addAll(Collections.nCopies(WATER_MIN_AMOUNT, TerrainType.WATER));
+
+        int remainingAmount = amount - terrainTypeQueue.size();
+        terrainTypeQueue.addAll(Collections.nCopies(remainingAmount, TerrainType.MOUNTAIN));
+
+        return terrainTypeQueue;
+    }
+
     public GameMap generateMap() {
-        // TODO: Implement a complete map generation algorithm
         Map<Position, GameMapNode> mapNodes = HashMap.newHashMap(MAP_SIZE);
 
         Position fortPosition = pickFortPosition();
         MapDirection fortEntranceDirection = MapDirection.randomDirection(random);
-        mapNodes.putAll(fortGenerator.generateFortStructure(fortPosition, fortEntranceDirection));
+        Map<Position, GameMapNode> fortStructureNodes = fortGenerator
+                .generateFortStructure(fortPosition, fortEntranceDirection);
+
+        mapNodes.putAll(fortStructureNodes);
+
+        int remainingMapNodes = MAP_SIZE - mapNodes.size();
+
+        Set<Position> positionQueue = generatePositionRange();
+        positionQueue.removeAll(mapNodes.keySet());
+
+        List<TerrainType> terrainTypeQueue = generateTerrainTypeQueue(remainingMapNodes);
+        Collections.shuffle(terrainTypeQueue, random);
+
+        Map<Position, GameMapNode> randomMapNodes = positionQueue.stream()
+                .map(position -> new GameMapNode(position, terrainTypeQueue.removeLast()))
+                .collect(Collectors.toMap(GameMapNode::getPosition, Function.identity()));
+
+        mapNodes.putAll(randomMapNodes);
 
         return new GameMap(mapNodes.values());
     }
