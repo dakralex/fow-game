dakralex/fow-game

commit 016f5c2703b5c9d52de66a642b1259bead4a3d18

client: map: implement update methods for game state, map and map nodes
Implements update methods for the classes `GameClientState`, `GameMap` and
`GameMapNode`, which are called like a cascade, i.e. the state updates the game
map, which in turn creates or updates each individual map node.

Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

Source/Client/src/main/java/client/map/GameMap.java
@@ -62,6 +62,14 @@ public class GameMap {
                 .orElse(Position.originPosition);
     }
 
+    public void update(GameMap newMap) {
+        newMap.nodes.forEach((position, newMapNode) -> {
+            GameMapNode mapNode = nodes.getOrDefault(position, newMapNode);
+
+            mapNode.update(newMapNode);
+        });
+    }
+
     public Set<Position> getPositions() {
         return Collections.unmodifiableSet(nodes.keySet());
     }

Source/Client/src/main/java/client/main/GameClientState.java
@@ -58,6 +58,15 @@ public class GameClientState {
         return new GameClientState(gameId, stateId, map, player, opponent);
     }
 
+    public void update(GameClientState newState) {
+        // Return early, if the new state has not nothing changed
+        if (stateId.equals(newState.stateId)) {
+            return;
+        }
+
+        map.update(newState.map);
+    }
+
     public boolean hasBothPlayers() {
         return opponent.isPresent();
     }

Source/Client/src/main/java/client/map/GameMapNode.java
@@ -47,6 +47,36 @@ public class GameMapNode implements Comparable<GameMapNode> {
                                      terrainType.intoETerrain());
     }
 
+    private void throwOnInvalidUpdatePosition(GameMapNode newGameMapNode) {
+        String errorMessage = String.format("GameMapNode at %s was updated with new one at %s.",
+                                            position,
+                                            newGameMapNode.position);
+
+        throw new IllegalArgumentException(errorMessage);
+    }
+
+    private void throwOnInvalidUpdateTerrain(GameMapNode newGameMapNode) {
+        String errorMessage = String.format("GameMapNode at %s changed terrain from %s to %s",
+                                            position,
+                                            terrainType,
+                                            newGameMapNode.terrainType);
+
+        throw new IllegalArgumentException(errorMessage);
+    }
+
+    public void update(GameMapNode newGameMapNode) {
+        if (!position.equals(newGameMapNode.position)) {
+            throwOnInvalidUpdatePosition(newGameMapNode);
+        }
+
+        if (!terrainType.equals(newGameMapNode.terrainType)) {
+            throwOnInvalidUpdateTerrain(newGameMapNode);
+        }
+
+        fortState = newGameMapNode.fortState;
+        treasureState = newGameMapNode.treasureState;
+    }
+
     public Position getPosition() {
         return position;
     }

Source/Client/src/main/java/client/map/Position.java
@@ -35,4 +35,9 @@ public record Position(int x, int y) implements Comparable<Position> {
     public int hashCode() {
         return Objects.hash(x, y);
     }
+
+    @Override
+    public String toString() {
+        return String.format("(%s, %s)", x, y);
+    }
 }

commit e3102adc2a75e61c9fc71b475ff91b14a4deb793

client: map: add new map nodes on game map updates
Fixes a bug introduced in 016f5c2, which caused the `GameMap.update` method to
not put any new map nodes in the `GameMap` instance, i.e. GameMaps could never
be filled on an update.

Fixes: 016f5c2 ("client: map: implement update methods for game state, map and map nodes")
Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

Source/Client/src/main/java/client/map/GameMap.java
@@ -51,13 +51,15 @@ public class GameMap {
 
     public void update(GameMap newMap) {
         newMap.nodes.forEach((position, newMapNode) -> {
-            if (!nodes.containsKey(position)) {
+            if (nodes.containsKey(position)) {
+                GameMapNode mapNode = nodes.get(position);
+
+                mapNode.update(newMapNode);
+            } else {
+                nodes.put(position, newMapNode);
+
                 logger.debug("Update adds a new GameMapNode at {}", position);
             }
-
-            GameMapNode mapNode = nodes.getOrDefault(position, newMapNode);
-
-            mapNode.update(newMapNode);
         });
     }
 