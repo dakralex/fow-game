dakralex/fow-game

commit af250300916bf9962eecad6056b8b6645217541c

client: validation: implement trivial half map validators
Implements the trivial half map validators to verify, that a generated half map
is compliant to the game's rules. The trivial validation cases are:

The half map...
- ...must contain exactly 50 map nodes,
- ...nodes must be within the x values 0 and 9,
- ...nodes must be within the y values 0 and 4,
- ...must have at least 24 map nodes with grass terrain [0],
- ...must have at least 5 map nodes with mountain terrain [1],
- ...must have at least 7 map nodes with water terrain [2], and
- ...must have at least 14 accessible border map nodes [3].

This implements a loop, which will generate maps until a valid map has been
generated to allow a fast, trivial random map generator to be implemented.

[0] 50 * 0.48 = 24
[1] 50 * 0.10 = 5
[2] 50 * 0.14 = 7
[3] ceil( (50 - 8 * 3) * 0.51 ) = 14, where 8 * 3 is the map without border.

Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

Source/Client/src/main/java/client/map/PositionArea.java
@@ -1,0 +1,21 @@
+package client.map;
+
+public record PositionArea(int x, int y, int width, int height) {
+    public boolean isInside(Position position) {
+        int posX = position.x();
+        int posY = position.y();
+
+        return posX >= x && posX < (x + width) && posY >= y && posY < (y + height);
+    }
+
+    public boolean isOnBorder(Position position) {
+        int posX = position.x();
+        int posY = position.y();
+
+        return posX == x || posX == (x + width - 1) || posY == y || posY == (y + height - 1);
+    }
+
+    public boolean isOnBorder(GameMapNode mapNode) {
+        return isOnBorder(mapNode.getPosition());
+    }
+}

Source/Client/src/main/java/client/validation/HalfMapValidator.java
@@ -1,0 +1,64 @@
+package client.validation;
+
+import java.util.Collections;
+import java.util.List;
+
+import client.map.GameMap;
+import client.map.GameMapNode;
+import client.map.PositionArea;
+import client.map.TerrainType;
+
+public class HalfMapValidator {
+
+    private static final int X_SIZE = 10;
+    private static final int Y_SIZE = 5;
+    private static final int MAP_SIZE = X_SIZE * Y_SIZE;
+
+    private static final int GRASS_MIN_AMOUNT = 24;
+    private static final int MOUNTAIN_MIN_AMOUNT = 5;
+    private static final int WATER_MIN_AMOUNT = 7;
+
+    private static final int BORDER_ACCESSIBLE_MIN_AMOUNT = 14;
+
+    private boolean validateSize(GameMap map) {
+        return map.getSize() == MAP_SIZE;
+    }
+
+    private boolean validatePositions(GameMap map) {
+        PositionArea area = new PositionArea(0, 0, X_SIZE, Y_SIZE);
+
+        return map.getPositions().stream().allMatch(area::isInside);
+    }
+
+    private boolean validateTerrainDistribution(GameMap map) {
+        List<TerrainType> terrains = map.getMapNodes().stream()
+                .map(GameMapNode::getTerrainType)
+                .toList();
+
+        int grassCount = Collections.frequency(terrains, TerrainType.GRASS);
+        int mountainCount = Collections.frequency(terrains, TerrainType.MOUNTAIN);
+        int waterCount = Collections.frequency(terrains, TerrainType.WATER);
+
+        return grassCount >= GRASS_MIN_AMOUNT
+                && mountainCount >= MOUNTAIN_MIN_AMOUNT
+                && waterCount >= WATER_MIN_AMOUNT;
+    }
+
+    private boolean validateBorderAccessibility(GameMap map) {
+        PositionArea area = new PositionArea(0, 0, X_SIZE, Y_SIZE);
+
+        long accessibleMapNodeCount = map.getMapNodes().stream()
+                .filter(area::isOnBorder)
+                .filter(GameMapNode::isAccessible)
+                .count();
+
+        return accessibleMapNodeCount >= BORDER_ACCESSIBLE_MIN_AMOUNT;
+    }
+
+    public boolean validate(GameMap map) {
+        return validateSize(map)
+                && validatePositions(map)
+                && validateTerrainDistribution(map)
+                && validateBorderAccessibility(map);
+    }
+}

Source/Client/src/main/java/client/generation/MapGenerator.java
@@ -8,6 +8,7 @@ import client.map.GameMap;
 import client.map.GameMapNode;
 import client.map.MapDirection;
 import client.map.Position;
+import client.validation.HalfMapValidator;
 
 public class MapGenerator {
 
@@ -42,5 +43,15 @@ public class MapGenerator {
         mapNodes.putAll(fortGenerator.generateFortStructure(fortPosition, fortEntranceDirection));
 
         return new GameMap(mapNodes.values());
+    }
+
+    public GameMap generateUntilValid(HalfMapValidator validator) {
+        GameMap map = generateMap();
+
+        while (!validator.validate(map)) {
+            map = generateMap();
+        }
+
+        return map;
     }
 }

Source/Client/src/main/java/client/main/MainClient.java
@@ -9,6 +9,7 @@ import client.network.GameMapSender;
 import client.network.GameServerClient;
 import client.network.GameStateUpdater;
 import client.player.PlayerDetails;
+import client.validation.HalfMapValidator;
 
 public class MainClient {
 
@@ -47,7 +48,8 @@ public class MainClient {
         // TODO: Implement an actual map generation algorithm, this will fail without that
         GameMapSender mapSender = new GameMapSender(serverClient, token);
         MapGenerator mapGenerator = new MapGenerator();
-        GameMap gameMap = mapGenerator.generateMap();
+        HalfMapValidator mapValidator = new HalfMapValidator();
+        GameMap gameMap = mapGenerator.generateUntilValid(mapValidator);
 
         System.out.println(gameMap);
 

Source/Client/src/main/java/client/map/GameMapNode.java
@@ -59,6 +59,10 @@ public class GameMapNode implements Comparable<GameMapNode> {
         return terrainType;
     }
 
+    public boolean isAccessible() {
+        return terrainType == TerrainType.GRASS || terrainType == TerrainType.MOUNTAIN;
+    }
+
     public boolean hasPlayerFort() {
         return fortState == FortState.PLAYER_FORT_PRESENT;
     }

commit 6c6500dc58697ec247ce35c9290dd244b5b38aa1

client: validation: fix border accessibility validator
Implements the border accessibility validator as a validation rule, where the
border nodes in each border direction must contain at least 51 % accessible
nodes to be accepted as a correctly generated `GameMap`.

This fixes the behavior of the border accessibility validator implemented in
af25030, which validates whether the whole player half map's border contains at
least 51 % (i.e. 14) accessible map nodes. This is wrong and will result in the
server rejecting the sent map.

Fixes: af25030 ("client: validation: implement trivial half map validators")
Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

Source/Client/src/main/java/client/map/PositionArea.java
@@ -8,14 +8,19 @@ public record PositionArea(int x, int y, int width, int height) {
         return posX >= x && posX < (x + width) && posY >= y && posY < (y + height);
     }
 
-    private boolean isOnBorder(Position position) {
-        int posX = position.x();
-        int posY = position.y();
-
-        return posX == x || posX == (x + width - 1) || posY == y || posY == (y + height - 1);
+    public boolean isOnEastBorder(Position position) {
+        return position.x() == x + width - 1;
     }
 
-    public boolean isOnBorder(GameMapNode mapNode) {
-        return isOnBorder(mapNode.getPosition());
+    public boolean isOnNorthBorder(Position position) {
+        return position.y() == y;
+    }
+
+    public boolean isOnSouthBorder(Position position) {
+        return position.y() == y + height - 1;
+    }
+
+    public boolean isOnWestBorder(Position position) {
+        return position.x() == x;
     }
 }

Source/Client/src/main/java/client/validation/HalfMapValidator.java
@@ -1,13 +1,16 @@
 package client.validation;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import client.map.GameMap;
 import client.map.GameMapNode;
+import client.map.MapDirection;
 import client.map.Position;
 import client.map.PositionArea;
 import client.map.TerrainType;
@@ -22,7 +25,7 @@ public class HalfMapValidator {
     private static final int MOUNTAIN_MIN_AMOUNT = 5;
     private static final int WATER_MIN_AMOUNT = 7;
 
-    private static final int BORDER_ACCESSIBLE_MIN_AMOUNT = 14;
+    private static final int BORDER_ACCESS_MIN_PERCENTAGE = 51;
 
     private boolean validateSize(GameMap map) {
         return map.getSize() == MAP_SIZE;
@@ -48,15 +51,26 @@ public class HalfMapValidator {
                 && waterCount >= WATER_MIN_AMOUNT;
     }
 
+    private boolean validateEachBorderAccessibility(GameMap map, MapDirection direction) {
+        PositionArea mapArea = new PositionArea(0, 0, X_SIZE, Y_SIZE);
+        Predicate<GameMapNode> isOnBorder = switch (direction) {
+            case EAST -> mapNode -> mapArea.isOnEastBorder(mapNode.getPosition());
+            case NORTH -> mapNode -> mapArea.isOnNorthBorder(mapNode.getPosition());
+            case SOUTH -> mapNode -> mapArea.isOnSouthBorder(mapNode.getPosition());
+            case WEST -> mapNode -> mapArea.isOnWestBorder(mapNode.getPosition());
+        };
+        List<GameMapNode> borderNodes = map.getMapNodes().stream().filter(isOnBorder).toList();
+
+        long nodeCount = borderNodes.size();
+        long minAccessibleNodeCount = Math.ceilDiv(BORDER_ACCESS_MIN_PERCENTAGE * nodeCount, 100);
+        long accessibleNodeCount = borderNodes.stream().filter(GameMapNode::isAccessible).count();
+
+        return accessibleNodeCount >= minAccessibleNodeCount;
+    }
+
     private boolean validateBorderAccessibility(GameMap map) {
-        PositionArea area = new PositionArea(0, 0, X_SIZE, Y_SIZE);
-
-        long accessibleMapNodeCount = map.getMapNodes().stream()
-                .filter(area::isOnBorder)
-                .filter(GameMapNode::isAccessible)
-                .count();
-
-        return accessibleMapNodeCount >= BORDER_ACCESSIBLE_MIN_AMOUNT;
+        return Arrays.stream(MapDirection.values())
+                .allMatch(direction -> validateEachBorderAccessibility(map, direction));
     }
 
     private boolean validateFortPlacement(GameMap map) {