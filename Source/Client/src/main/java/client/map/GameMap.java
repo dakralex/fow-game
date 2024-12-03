dakralex/fow-game

commit c320e2d17640a82a4e76295eff80eba2768586db

client: map: implement debug rendering of player half map
Implements the debug rendering of `GameMap` class instances to a string by
displaying the 10 x 5 lattice, where each letter notates the terrain type of
the map node. For a map node containing a fort, this is the letter "F". For
other map nodes, this is "G" for grass terrain, "M" for mountain terrain and
"W" for water terrain.

Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

Source/Client/src/main/java/client/map/GameMap.java
@@ -1,6 +1,8 @@
 package client.map;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import messagesbase.messagesfromclient.PlayerHalfMap;
@@ -57,5 +59,30 @@ public class GameMap {
                 .collect(Collectors.toSet());
 
         return new PlayerHalfMap(playerId, halfMapNodes);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("  | 0 1 2 3 4 5 6 7 8 9\n");
+
+        Map<Position, GameMapNode> viewNodes = nodes.stream()
+                .collect(Collectors.toUnmodifiableMap(GameMapNode::getPosition, Function.identity()));
+
+        for (int y = 0; y < 5; ++y) {
+            stringBuilder.append(String.format("%s | ", y));
+            for (int x = 0; x < 10; ++x) {
+                Position currentPosition = new Position(x, y);
+
+                if (!viewNodes.containsKey(currentPosition)) {
+                    stringBuilder.append("a ");
+                } else {
+                    GameMapNode currentMapNode = viewNodes.get(currentPosition);
+                    stringBuilder.append(String.format("%s ", currentMapNode));
+                }
+            }
+            stringBuilder.append("\n");
+        }
+
+        return stringBuilder.toString();
     }
 }

Source/Client/src/main/java/client/main/MainClient.java
@@ -48,6 +48,9 @@ public class MainClient {
         GameMapSender mapSender = new GameMapSender(serverClient, token);
         MapGenerator mapGenerator = new MapGenerator();
         GameMap gameMap = mapGenerator.generateMap();
+
+        System.out.println(gameMap);
+
         mapSender.sendMap(gameMap);
 
 		System.out.println("My Player ID: " + token.playerId());

Source/Client/src/main/java/client/map/GameMapNode.java
@@ -64,4 +64,30 @@ public class GameMapNode {
     public boolean hasTreasure() {
         return treasureState == TreasureState.PLAYER_TREASURE_PRESENT;
     }
+
+    @Override
+    public String toString() {
+        if (hasPlayerFort()) {
+            return "F";
+        }
+
+        return terrainType.toString();
+    }
+
+    @Override
+    public int compareTo(GameMapNode other) {
+        return this.position.compareTo(other.position);
+    }
+
+    @Override
+    public final boolean equals(Object other) {
+        if (!(other instanceof GameMapNode otherMapNode)) return false;
+
+        return Objects.equals(position, otherMapNode.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(position);
+    }
 }

Source/Client/src/main/java/client/map/Position.java
@@ -1,12 +1,34 @@
 package client.map;
 
+import java.util.Objects;
+
 import messagesbase.messagesfromserver.FullMapNode;
 
-public record Position(int x, int y) {
+public record Position(int x, int y) implements Comparable<Position> {
 
     public static final Position originPosition = new Position(0, 0);
 
     public static Position fromFullMapNode(FullMapNode fullMapNode) {
         return new Position(fullMapNode.getX(), fullMapNode.getY());
+    }
+
+    @Override
+    public int compareTo(Position other) {
+        if (this.y == other.y) {
+            return this.x - other.x;
+        }
+
+        return this.y - other.y;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof Position(int x, int y))) return false;
+        return this.x == x && this.y == y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }

Source/Client/src/main/java/client/map/TerrainType.java
@@ -22,4 +22,13 @@ public enum TerrainType {
             case WATER -> ETerrain.Water;
         };
     }
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case GRASS -> "G";
+            case MOUNTAIN -> "M";
+            case WATER -> "W";
+        };
+    }
 }

commit 6ee1fefdeee65a058f00cd59543cf2fac4baa940

client: map: make `GameMap.toString` output any map size
Adapt `GameMap.toString` to output the game map for any possible size instead
of the previously fixed size of a player's half map (i.e. 10 x 5).

Fixes: c320e2d ("client: map: implement debug rendering of player half map")
Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

Source/Client/src/main/java/client/map/GameMap.java
@@ -12,6 +12,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import messagesbase.messagesfromclient.PlayerHalfMap;
@@ -90,11 +91,17 @@ public class GameMap {
 
     @Override
     public String toString() {
-        StringBuilder stringBuilder = new StringBuilder("  | 0 1 2 3 4 5 6 7 8 9\n");
+        StringBuilder stringBuilder = new StringBuilder("   |");
 
-        for (int y = 0; y < 5; ++y) {
-            stringBuilder.append(String.format("%s | ", y));
-            for (int x = 0; x < 10; ++x) {
+        PositionArea area = getArea();
+        IntStream.range(0, area.width())
+                .forEach(index -> stringBuilder.append(String.format(" %d", index % 10)));
+
+        stringBuilder.append("\n");
+
+        for (int y = 0; y < area.height(); ++y) {
+            stringBuilder.append(String.format("%2d | ", y));
+            for (int x = 0; x < area.width(); ++x) {
                 Position currentPosition = new Position(x, y);
 
                 if (!nodes.containsKey(currentPosition)) {