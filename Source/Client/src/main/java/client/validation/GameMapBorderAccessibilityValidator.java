02944a94fff27551d267a9c167b981f1218bdcaf
Daniel Kral <a11908284@unet.univie.ac.at>
2025-01-02 20:39:38 +0100

    client: validation: improve readability of `GameMapBorderAccessibilityValidator`
    Improves the readability of the class' methods and control flow by:
    
    - factoring the `isOn*Border` predicate switch statement out,
    - fixing the implicit number type cast from `int` to `long`, and
    - inverting the validation condition in the `validate` method.
    
    Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

--- Source/Client/src/main/java/client/validation/GameMapBorderAccessibilityValidator.java
+++ Source/Client/src/main/java/client/validation/GameMapBorderAccessibilityValidator.java
@@ -16,18 +16,23 @@ public class GameMapBorderAccessibilityValidator implements GameMapValidationRul
 
     private static final int BORDER_ACCESS_MIN_PERCENTAGE = 51;
 
-    private boolean validateEachBorderAccessibility(GameMap map, MapDirection direction) {
+    private static Predicate<GameMapNode> getBorderPredicate(MapDirection direction) {
         PositionArea mapArea = new PositionArea(0, 0, X_SIZE, Y_SIZE);
-        Predicate<GameMapNode> isOnBorder = switch (direction) {
+
+        return switch (direction) {
             case EAST -> mapNode -> mapArea.isOnEastBorder(mapNode.getPosition());
             case NORTH -> mapNode -> mapArea.isOnNorthBorder(mapNode.getPosition());
             case SOUTH -> mapNode -> mapArea.isOnSouthBorder(mapNode.getPosition());
             case WEST -> mapNode -> mapArea.isOnWestBorder(mapNode.getPosition());
         };
+    }
+
+    private static boolean isBorderAccessible(GameMap map, MapDirection direction) {
+        Predicate<GameMapNode> isOnBorder = getBorderPredicate(direction);
         List<GameMapNode> borderNodes = map.getMapNodes().stream().filter(isOnBorder).toList();
 
-        long nodeCount = borderNodes.size();
-        long minAccessibleNodeCount = Math.ceilDiv(BORDER_ACCESS_MIN_PERCENTAGE * nodeCount, 100);
+        int nodeCount = borderNodes.size();
+        int minAccessibleNodeCount = Math.ceilDiv(BORDER_ACCESS_MIN_PERCENTAGE * nodeCount, 100);
         long accessibleNodeCount = borderNodes.stream().filter(GameMapNode::isAccessible).count();
 
         return accessibleNodeCount >= minAccessibleNodeCount;
@@ -35,10 +40,10 @@ public class GameMapBorderAccessibilityValidator implements GameMapValidationRul
 
     @Override
     public void validate(GameMap map, Notification<? super GameMapValidationRule> note) {
-        boolean allBordersAccessibleEnough = Arrays.stream(MapDirection.values())
-                .allMatch(direction -> validateEachBorderAccessibility(map, direction));
+        boolean areBordersInaccessible = !Arrays.stream(MapDirection.values())
+                .allMatch(direction -> isBorderAccessible(map, direction));
 
-        if (!allBordersAccessibleEnough) {
+        if (areBordersInaccessible) {
             note.addEntry(this,
                           String.format("Game map has a border, where less than %d %% is accessible",
                                         BORDER_ACCESS_MIN_PERCENTAGE));
