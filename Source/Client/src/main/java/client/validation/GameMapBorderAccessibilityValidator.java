542be770086c034753f206154385379e6913aa13
Daniel Kral <a11908284@unet.univie.ac.at>
2025-01-02 12:36:46 +0100

    client: validation: factor out game map border accessibility validator
    Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

---
+++ Source/Client/src/main/java/client/validation/GameMapBorderAccessibilityValidator.java
@@ -0,0 +1,41 @@
+package client.validation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import client.map.GameMap;
+import client.map.GameMapNode;
+import client.map.MapDirection;
+import client.map.PositionArea;
+
+public class GameMapBorderAccessibilityValidator implements GameMapValidationRule {
+
+    private static final int X_SIZE = 10;
+    private static final int Y_SIZE = 5;
+
+    private static final int BORDER_ACCESS_MIN_PERCENTAGE = 51;
+
+    private boolean validateEachBorderAccessibility(GameMap map, MapDirection direction) {
+        PositionArea mapArea = new PositionArea(0, 0, X_SIZE, Y_SIZE);
+        Predicate<GameMapNode> isOnBorder = switch (direction) {
+            case EAST -> mapNode -> mapArea.isOnEastBorder(mapNode.getPosition());
+            case NORTH -> mapNode -> mapArea.isOnNorthBorder(mapNode.getPosition());
+            case SOUTH -> mapNode -> mapArea.isOnSouthBorder(mapNode.getPosition());
+            case WEST -> mapNode -> mapArea.isOnWestBorder(mapNode.getPosition());
+        };
+        List<GameMapNode> borderNodes = map.getMapNodes().stream().filter(isOnBorder).toList();
+
+        long nodeCount = borderNodes.size();
+        long minAccessibleNodeCount = Math.ceilDiv(BORDER_ACCESS_MIN_PERCENTAGE * nodeCount, 100);
+        long accessibleNodeCount = borderNodes.stream().filter(GameMapNode::isAccessible).count();
+
+        return accessibleNodeCount >= minAccessibleNodeCount;
+    }
+
+    @Override
+    public boolean validate(GameMap map) {
+        return Arrays.stream(MapDirection.values())
+                .allMatch(direction -> validateEachBorderAccessibility(map, direction));
+    }
+}
--- Source/Client/src/main/java/client/validation/HalfMapValidator.java
+++ Source/Client/src/main/java/client/validation/HalfMapValidator.java
@@ -1,47 +1,16 @@
 package client.validation;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import client.map.GameMap;
 import client.map.GameMapNode;
-import client.map.MapDirection;
 import client.map.Position;
-import client.map.PositionArea;
 import client.map.TerrainType;
 
 public class HalfMapValidator {
-
-    private static final int X_SIZE = 10;
-    private static final int Y_SIZE = 5;
-
-    private static final int BORDER_ACCESS_MIN_PERCENTAGE = 51;
-
-    private boolean validateEachBorderAccessibility(GameMap map, MapDirection direction) {
-        PositionArea mapArea = new PositionArea(0, 0, X_SIZE, Y_SIZE);
-        Predicate<GameMapNode> isOnBorder = switch (direction) {
-            case EAST -> mapNode -> mapArea.isOnEastBorder(mapNode.getPosition());
-            case NORTH -> mapNode -> mapArea.isOnNorthBorder(mapNode.getPosition());
-            case SOUTH -> mapNode -> mapArea.isOnSouthBorder(mapNode.getPosition());
-            case WEST -> mapNode -> mapArea.isOnWestBorder(mapNode.getPosition());
-        };
-        List<GameMapNode> borderNodes = map.getMapNodes().stream().filter(isOnBorder).toList();
-
-        long nodeCount = borderNodes.size();
-        long minAccessibleNodeCount = Math.ceilDiv(BORDER_ACCESS_MIN_PERCENTAGE * nodeCount, 100);
-        long accessibleNodeCount = borderNodes.stream().filter(GameMapNode::isAccessible).count();
-
-        return accessibleNodeCount >= minAccessibleNodeCount;
-    }
-
-    private boolean validateBorderAccessibility(GameMap map) {
-        return Arrays.stream(MapDirection.values())
-                .allMatch(direction -> validateEachBorderAccessibility(map, direction));
-    }
 
     private boolean validateFortPlacement(GameMap map) {
         List<GameMapNode> fortNodes = map.getMapNodes().stream()
@@ -93,11 +62,12 @@ public class HalfMapValidator {
         GameMapSizeValidator mapSizeValidator = new GameMapSizeValidator();
         GameMapPositionsValidator mapPositionsValidator = new GameMapPositionsValidator();
         GameMapTerrainDistributionValidator mapTerrainDistributionValidator = new GameMapTerrainDistributionValidator();
+        GameMapBorderAccessibilityValidator mapBorderAccessibilityValidator = new GameMapBorderAccessibilityValidator();
 
         return mapSizeValidator.validate(map)
                 && mapPositionsValidator.validate(map)
                 && mapTerrainDistributionValidator.validate(map)
-                && validateBorderAccessibility(map)
+                && mapBorderAccessibilityValidator.validate(map)
                 && validateFortPlacement(map)
                 && validateTreasurePlacement(map)
                 && validateTerrainReachability(map);
