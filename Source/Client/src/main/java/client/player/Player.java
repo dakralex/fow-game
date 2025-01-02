c44f3e5b0d2f800a83dc611e0184de4c2a12fdca
Daniel Kral <a11908284@unet.univie.ac.at>
2025-01-02 20:32:25 +0100

    client: player: remove unused short-arg constructor for `Player`
    Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

--- Source/Client/src/main/java/client/player/Player.java
+++ Source/Client/src/main/java/client/player/Player.java
@@ -20,13 +20,6 @@ public class Player {
     private Position position;
     private boolean hasTreasure;
 
-    private Player(String playerId, PlayerDetails details) {
-        this.playerId = playerId;
-        this.details = details;
-        this.position = new Position(0, 0);
-        this.hasTreasure = false;
-    }
-
     private Player(String playerId, PlayerDetails details, PlayerGameState state,
                    Position position, boolean hasTreasure) {
         this.playerId = playerId;
