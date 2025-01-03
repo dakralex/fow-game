48fd07b5c891468d275897e41f4ed222ce4d53a8
Daniel Kral <a11908284@unet.univie.ac.at>
2025-01-03 17:19:12 +0100

    Red: client: test: map: implement test cases for `PositionArea::intoPositionStream`
    Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

--- Source/Client/src/test/java/client/map/PositionAreaTest.java
+++ Source/Client/src/test/java/client/map/PositionAreaTest.java
@@ -1,11 +1,23 @@
 package client.map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Collection;
+import java.util.stream.Stream;
+
 class PositionAreaTest {
+
+    private static final int HALF_MAP_X_SIZE = 10;
+    private static final int HALF_MAP_Y_SIZE = 5;
+    private static final int HALF_MAP_SIZE = HALF_MAP_X_SIZE * HALF_MAP_Y_SIZE;
 
     @ParameterizedTest
     @CsvSource({"5,5", "10,10", "5,10", "10, 5", "7,7"})
@@ -21,5 +33,48 @@ class PositionAreaTest {
         PositionArea area = new PositionArea(5, 5, 10, 10);
 
         assertTrue(area.isOutside(new Position(x, y)), "Position should be considered as outside");
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(HalfMapArgumentProvider.class)
+    void HalfMaps_intoPositionStream_shouldOutputCompleteHalfMapPositionStream(int x, int y,
+                                                                               int width,
+                                                                               int height) {
+        PositionArea area = new PositionArea(x, y, width, height);
+        Collection<Position> areaPositions = area.intoPositionStream().toList();
+
+        assertEquals(HALF_MAP_SIZE,
+                     areaPositions.size(),
+                     "HalfMap should contain the excepted GameMapNodes");
+
+        int x1 = x + width - 1;
+        int y1 = y + width - 1;
+
+        Collection<Position> positionsOutsideXBound = areaPositions.stream()
+                .filter(position -> position.x() < x || position.x() > x1)
+                .toList();
+        Collection<Position> positionsOutsideYBound = areaPositions.stream()
+                .filter(position -> position.y() < y || position.y() > y1)
+                .toList();
+
+        assertTrue(positionsOutsideXBound.isEmpty(),
+                   "Positions should be in the x coordinate range");
+        assertTrue(positionsOutsideYBound.isEmpty(),
+                   "Positions should be in the y coordinate range");
+    }
+
+    private static class HalfMapArgumentProvider implements ArgumentsProvider {
+
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
+            return Stream.of(
+                    // Horizontal west / Vertical north half map
+                    arguments(0, 0, HALF_MAP_X_SIZE, HALF_MAP_Y_SIZE),
+                    // Vertical south half map
+                    arguments(0, HALF_MAP_Y_SIZE, HALF_MAP_X_SIZE, HALF_MAP_Y_SIZE),
+                    // Horizontal east half map
+                    arguments(HALF_MAP_X_SIZE, 0, HALF_MAP_X_SIZE, HALF_MAP_Y_SIZE)
+            );
+        }
     }
 }
