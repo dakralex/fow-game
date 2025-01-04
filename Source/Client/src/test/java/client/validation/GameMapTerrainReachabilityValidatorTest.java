9de09026e688b380160c5c83ad3481d0f7d9beb2
Daniel Kral <a11908284@unet.univie.ac.at>
2025-01-04 21:41:31 +0100

    client: test: validation: implement test cases for `GameMapTerrainReachabilityValidator`
    Implements the two core test cases for the player's half map terrain
    reachability validator, to verify that it correctly reports:
    
    - that an empty (i.e. only grass terrain fields) is correct, and
    - that an isolated islands (i.e. big patch of grass terrain fields, surrounded
      by water an all sides, but the corners) is incorrect.
    
    Just for an illustration to clarify the isolated island (G = Grass, W = Water):
    
    G W W ... W W G
    W G G ... G G W
    . . . ... . . .
    W G G ... G G W
    G W W ... W W G
    
    Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

---
+++ Source/Client/src/test/java/client/validation/GameMapTerrainReachabilityValidatorTest.java
@@ -0,0 +1,63 @@
+package client.validation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.function.Function;
+
+import client.map.GameMap;
+import client.map.GameMapNode;
+import client.map.MapDirection;
+import client.map.Position;
+import client.map.PositionArea;
+import client.map.TerrainType;
+import client.map.util.MapGenerationUtils;
+import client.validation.util.NotificationAssertUtils;
+
+class GameMapTerrainReachabilityValidatorTest {
+
+    private static final int X_SIZE = 10;
+    private static final int Y_SIZE = 5;
+
+    private static final GameMapValidationRule validator = new GameMapTerrainReachabilityValidator();
+
+    private static Function<GameMapNode, GameMapNode> makeIsolatedIsland(PositionArea mapArea) {
+        Collection<Position> allPositions = mapArea.intoPositionStream().toList();
+
+        Collection<Position> borderNodesExceptCorners = Arrays.stream(MapDirection.values())
+                .map(mapArea::getBorderPredicate)
+                .flatMap(borderPredicate -> allPositions.stream().filter(borderPredicate))
+                .toList();
+
+        return mapNode -> {
+            Position mapNodePosition = mapNode.getPosition();
+            if (borderNodesExceptCorners.contains(mapNodePosition)) {
+                return new GameMapNode(mapNode.getPosition(), TerrainType.WATER);
+            } else {
+                return mapNode;
+            }
+        };
+    }
+
+    @Test
+    void EmptyMap_validate_shouldMarkAsValid() {
+        GameMap map = MapGenerationUtils.generateEmptyGameMap(X_SIZE, Y_SIZE, MapDirection.EAST);
+
+        NotificationAssertUtils.assertNoViolation(map, validator);
+    }
+
+    @Test
+    void BigIslandMap_validate_shouldMarkAsInvalid() {
+        PositionArea mapArea = new PositionArea(0, 0, X_SIZE, Y_SIZE);
+
+        GameMap map = MapGenerationUtils.generateEmptyGameMap(X_SIZE,
+                                                              Y_SIZE,
+                                                              makeIsolatedIsland(mapArea),
+                                                              GameMapNode::isLootable);
+
+        System.err.println(map);
+
+        NotificationAssertUtils.assertSomeViolation(map, validator);
+    }
+}
