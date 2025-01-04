dakralex/fow-game

commit 9d3a13d55bcbe89a713e59ed211c2f43a3606a6f

client: validation: factor out game map fort placement validator
Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

Source/Client/src/main/java/client/validation/GameMapFortPlacementValidator.java
@@ -1,0 +1,26 @@
+package client.validation;
+
+import java.util.List;
+
+import client.map.GameMap;
+import client.map.GameMapNode;
+import client.map.TerrainType;
+
+public class GameMapFortPlacementValidator implements GameMapValidationRule {
+
+    @Override
+    public boolean validate(GameMap map) {
+        List<GameMapNode> fortNodes = map.getMapNodes().stream()
+                .filter(GameMapNode::hasPlayerFort)
+                .toList();
+
+        if (fortNodes.size() != 1) {
+            return false;
+        }
+
+        GameMapNode fortMapNode = fortNodes.getFirst();
+
+        return !fortMapNode.hasOpponentFort()
+                && fortMapNode.getTerrainType() == TerrainType.GRASS;
+    }
+}

Source/Client/src/main/java/client/validation/HalfMapValidator.java
@@ -1,31 +1,14 @@
 package client.validation;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import client.map.GameMap;
 import client.map.GameMapNode;
 import client.map.Position;
-import client.map.TerrainType;
 
 public class HalfMapValidator {
-
-    private boolean validateFortPlacement(GameMap map) {
-        List<GameMapNode> fortNodes = map.getMapNodes().stream()
-                .filter(GameMapNode::hasPlayerFort)
-                .toList();
-
-        if (fortNodes.size() != 1) {
-            return false;
-        }
-
-        GameMapNode fortMapNode = fortNodes.getFirst();
-
-        return !fortMapNode.hasOpponentFort()
-                && fortMapNode.getTerrainType() == TerrainType.GRASS;
-    }
 
     private boolean validateTreasurePlacement(GameMap map) {
         return map.getMapNodes().stream().noneMatch(GameMapNode::hasTreasure);
@@ -63,12 +46,13 @@ public class HalfMapValidator {
         GameMapPositionsValidator mapPositionsValidator = new GameMapPositionsValidator();
         GameMapTerrainDistributionValidator mapTerrainDistributionValidator = new GameMapTerrainDistributionValidator();
         GameMapBorderAccessibilityValidator mapBorderAccessibilityValidator = new GameMapBorderAccessibilityValidator();
+        GameMapFortPlacementValidator mapFortPlacementValidator = new GameMapFortPlacementValidator();
 
         return mapSizeValidator.validate(map)
                 && mapPositionsValidator.validate(map)
                 && mapTerrainDistributionValidator.validate(map)
                 && mapBorderAccessibilityValidator.validate(map)
-                && validateFortPlacement(map)
+                && mapFortPlacementValidator.validate(map)
                 && validateTreasurePlacement(map)
                 && validateTerrainReachability(map);
     }

commit 624185cce7f9837d73a8f960450fc2048d7899ef

Green: client: validation: fix validator catching enemy fort placement
Fixes one of two failing test cases for the `GameMapFortPlacementValidator`,
where this fixes the validator to detect `GameMapNode`s with an enemy fort
placed on it and mark the business rule violation.

Fixes: 9d3a13d ("client: validation: factor out game map fort placement validator")
Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

Source/Client/src/main/java/client/map/GameMapNode.java
@@ -196,6 +196,10 @@ public class GameMapNode implements Comparable<GameMapNode> {
         return fortState == FortState.ENEMY_FORT_PRESENT;
     }
 
+    public boolean hasAnyFort() {
+        return hasPlayerFort() || hasEnemyFort();
+    }
+
     public boolean hasTreasure() {
         return treasureState == TreasureState.PLAYER_TREASURE_PRESENT;
     }

Source/Client/src/main/java/client/validation/GameMapFortPlacementValidator.java
@@ -11,7 +11,7 @@ public class GameMapFortPlacementValidator implements GameMapValidationRule {
     @Override
     public void validate(GameMap map, Notification<? super GameMapValidationRule> note) {
         List<GameMapNode> fortNodes = map.getMapNodes().stream()
-                .filter(GameMapNode::hasPlayerFort)
+                .filter(GameMapNode::hasAnyFort)
                 .toList();
 
         if (fortNodes.size() != 1) {