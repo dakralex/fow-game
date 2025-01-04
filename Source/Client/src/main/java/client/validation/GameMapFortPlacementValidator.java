4d969ca2616cc9e009b5cedb2ca71d75ce4292cf
Daniel Kral <a11908284@unet.univie.ac.at>
2025-01-04 11:14:48 +0100

    Green: client: validation: fix `NoSuchElementException` for fort placement validator
    Fixes a `NoSuchElementException` error while running the fort placement
    validation as the validation was made fall-through when implementing the
    notification pattern in the bad commit and the previous early-return control
    flow wasn't addressed correctly in this change.
    
    Fixes: 38f0ca8 ("client: validation: refactor game map validator to use notification pattern")
    Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

--- Source/Client/src/main/java/client/validation/GameMapFortPlacementValidator.java
+++ Source/Client/src/main/java/client/validation/GameMapFortPlacementValidator.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import client.map.GameMap;
 import client.map.GameMapNode;
-import client.map.TerrainType;
 
 public class GameMapFortPlacementValidator implements GameMapValidationRule {
 
@@ -14,18 +13,16 @@ public class GameMapFortPlacementValidator implements GameMapValidationRule {
                 .filter(GameMapNode::hasAnyFort)
                 .toList();
 
+        if (!fortNodes.stream().allMatch(GameMapNode::isLootable)) {
+            note.addEntry(this, "Game map has at least one non-grass field with a fort");
+        }
+
         if (fortNodes.stream().anyMatch(GameMapNode::hasEnemyFort)) {
             note.addEntry(this, "Game map has at least one field with the enemy's fort");
         }
 
         if (fortNodes.size() != 1) {
             note.addEntry(this, "Game map has no or more than one field with a fort");
-        }
-
-        GameMapNode fortMapNode = fortNodes.getFirst();
-
-        if (fortMapNode.getTerrainType() != TerrainType.GRASS) {
-            note.addEntry(this, "Game map has a non-grass field with the fort");
         }
     }
 }

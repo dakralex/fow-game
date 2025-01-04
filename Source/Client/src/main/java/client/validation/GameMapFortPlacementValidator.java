dakralex/fow-game

commit 38f0ca861781aa6be9232f69fe8ff4ccff956395

client: validation: refactor game map validator to use notification pattern
Implements the notification pattern in the `HalfMapValidator` class to allow
reporting of individual violation of business rules when validating a player's
half map during map generation.

This modifies the `GameMapValidationRule` API by adding to a `Notification<>`
instance instead of returning only a binary boolean value to allow more
clarification of what has gone wrong. The violations are listed in a
bullet-point list when logged as a string.

For most existent checks the conditions are split to accomodate a more granular
description of the business rule violation and are negated as the former
`validate(GameMap)` API returned `true` in case of success, but the
notification entry addition does only run for the `false` states now.

Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

Source/Client/src/main/java/client/validation/GameMapFortPlacementValidator.java
@@ -9,18 +9,23 @@ import client.map.TerrainType;
 public class GameMapFortPlacementValidator implements GameMapValidationRule {
 
     @Override
-    public boolean validate(GameMap map) {
+    public void validate(GameMap map, Notification<GameMapValidationRule> note) {
         List<GameMapNode> fortNodes = map.getMapNodes().stream()
                 .filter(GameMapNode::hasPlayerFort)
                 .toList();
 
         if (fortNodes.size() != 1) {
-            return false;
+            note.addEntry(this, "Game map has no or more than one field with a fort");
         }
 
         GameMapNode fortMapNode = fortNodes.getFirst();
 
-        return !fortMapNode.hasOpponentFort()
-                && fortMapNode.getTerrainType() == TerrainType.GRASS;
+        if (fortMapNode.getTerrainType() != TerrainType.GRASS) {
+            note.addEntry(this, "Game map has a non-grass field with the fort");
+        }
+
+        if (fortMapNode.hasOpponentFort()) {
+            note.addEntry(this, "Game map has a field with the opponent's fort");
+        }
     }
 }

Source/Client/src/main/java/client/generation/MapGenerator.java
@@ -18,7 +18,9 @@ import client.map.GameMapNode;
 import client.map.MapDirection;
 import client.map.Position;
 import client.map.TerrainType;
+import client.validation.GameMapValidationRule;
 import client.validation.HalfMapValidator;
+import client.validation.Notification;
 
 public class MapGenerator {
 
@@ -98,12 +100,19 @@ public class MapGenerator {
 
     public GameMap generateUntilValid(HalfMapValidator validator) {
         int generateTries = 0;
-        GameMap map = generateMap();
+        GameMap map;
+        Notification<GameMapValidationRule> validationErrors;
 
-        while (!validator.validate(map)) {
-            map = generateMap();
+        do {
             ++generateTries;
-        }
+            map = generateMap();
+            validationErrors = validator.validate(map);
+
+            if (validationErrors.hasEntries()) {
+                logger.debug("The generated half map violates the following rules:\n{}",
+                             validationErrors);
+            }
+        } while(validationErrors.hasEntries());
 
         logger.debug("It took {} tries to generate a valid GameMap instance.", generateTries);
 

Source/Client/src/main/java/client/validation/GameMapBorderAccessibilityValidator.java
@@ -34,8 +34,14 @@ public class GameMapBorderAccessibilityValidator implements GameMapValidationRul
     }
 
     @Override
-    public boolean validate(GameMap map) {
-        return Arrays.stream(MapDirection.values())
+    public void validate(GameMap map, Notification<GameMapValidationRule> note) {
+        boolean allBordersAccessibleEnough = Arrays.stream(MapDirection.values())
                 .allMatch(direction -> validateEachBorderAccessibility(map, direction));
+
+        if (!allBordersAccessibleEnough) {
+            note.addEntry(this,
+                          String.format("Game map has a border, where less than %d %% is accessible",
+                                        BORDER_ACCESS_MIN_PERCENTAGE));
+        }
     }
 }

Source/Client/src/main/java/client/validation/GameMapPositionsValidator.java
@@ -9,9 +9,11 @@ public class GameMapPositionsValidator implements GameMapValidationRule {
     private static final int Y_SIZE = 5;
 
     @Override
-    public boolean validate(GameMap map) {
+    public void validate(GameMap map, Notification<GameMapValidationRule> note) {
         PositionArea area = new PositionArea(0, 0, X_SIZE, Y_SIZE);
 
-        return map.getPositions().stream().allMatch(area::isInside);
+        if (!map.getPositions().stream().allMatch(area::isInside)) {
+            note.addEntry(this, "Game Map contains fields, whose positions are out-of-bounds");
+        }
     }
 }

Source/Client/src/main/java/client/validation/GameMapSizeValidator.java
@@ -9,7 +9,11 @@ public class GameMapSizeValidator implements GameMapValidationRule {
     private static final int MAP_SIZE = X_SIZE * Y_SIZE;
 
     @Override
-    public boolean validate(GameMap map) {
-        return map.getSize() == MAP_SIZE;
+    public void validate(GameMap map, Notification<GameMapValidationRule> note) {
+        if (map.getSize() != MAP_SIZE) {
+            note.addEntry(this,
+                          String.format("Game map does contain less or more than %d fields",
+                                        MAP_SIZE));
+        }
     }
 }

Source/Client/src/main/java/client/validation/GameMapTerrainDistributionValidator.java
@@ -14,7 +14,7 @@ public class GameMapTerrainDistributionValidator implements GameMapValidationRul
     private static final int WATER_MIN_AMOUNT = 7;
 
     @Override
-    public boolean validate(GameMap map) {
+    public void validate(GameMap map, Notification<GameMapValidationRule> note) {
         List<TerrainType> terrains = map.getMapNodes().stream()
                 .map(GameMapNode::getTerrainType)
                 .toList();
@@ -23,8 +23,22 @@ public class GameMapTerrainDistributionValidator implements GameMapValidationRul
         int mountainCount = Collections.frequency(terrains, TerrainType.MOUNTAIN);
         int waterCount = Collections.frequency(terrains, TerrainType.WATER);
 
-        return grassCount >= GRASS_MIN_AMOUNT
-                && mountainCount >= MOUNTAIN_MIN_AMOUNT
-                && waterCount >= WATER_MIN_AMOUNT;
+        if (grassCount < GRASS_MIN_AMOUNT) {
+            note.addEntry(this,
+                          String.format("Game map contains less than %d grass fields",
+                                        GRASS_MIN_AMOUNT));
+        }
+
+        if (mountainCount < MOUNTAIN_MIN_AMOUNT) {
+            note.addEntry(this,
+                          String.format("Game map contains less than %d mountain fields",
+                                        MOUNTAIN_MIN_AMOUNT));
+        }
+
+        if (waterCount < WATER_MIN_AMOUNT) {
+            note.addEntry(this,
+                          String.format("Game map contains less than %d water fields",
+                                        WATER_MIN_AMOUNT));
+        }
     }
 }

Source/Client/src/main/java/client/validation/GameMapTerrainReachabilityValidator.java
@@ -19,7 +19,7 @@ public class GameMapTerrainReachabilityValidator implements GameMapValidationRul
     }
 
     @Override
-    public boolean validate(GameMap map) {
+    public void validate(GameMap map, Notification<GameMapValidationRule> note) {
         Position fortPosition = map.getMapNodes().stream()
                 .filter(GameMapNode::hasPlayerFort)
                 .map(GameMapNode::getPosition)
@@ -35,6 +35,9 @@ public class GameMapTerrainReachabilityValidator implements GameMapValidationRul
 
         floodFillMap(map, fortPosition, visitedNodes);
 
-        return visitedNodes.containsAll(accessiblePositions);
+        if (!visitedNodes.containsAll(accessiblePositions)) {
+            note.addEntry(this,
+                          "Game map does contain accessible fields, that cannot be reached (i.e. islands)");
+        }
     }
 }

Source/Client/src/main/java/client/validation/GameMapTreasurePlacementValidator.java
@@ -6,7 +6,9 @@ import client.map.GameMapNode;
 public class GameMapTreasurePlacementValidator implements GameMapValidationRule {
 
     @Override
-    public boolean validate(GameMap map) {
-        return map.getMapNodes().stream().noneMatch(GameMapNode::hasTreasure);
+    public void validate(GameMap map, Notification<GameMapValidationRule> note) {
+        if (map.getMapNodes().stream().anyMatch(GameMapNode::hasTreasure)) {
+            note.addEntry(this, "Game map has at least one field with a treasure");
+        }
     }
 }

commit 4d969ca2616cc9e009b5cedb2ca71d75ce4292cf

Green: client: validation: fix `NoSuchElementException` for fort placement validator
Fixes a `NoSuchElementException` error while running the fort placement
validation as the validation was made fall-through when implementing the
notification pattern in the bad commit and the previous early-return control
flow wasn't addressed correctly in this change.

Fixes: 38f0ca8 ("client: validation: refactor game map validator to use notification pattern")
Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

Source/Client/src/main/java/client/validation/GameMapFortPlacementValidator.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import client.map.GameMap;
 import client.map.GameMapNode;
-import client.map.TerrainType;
 
 public class GameMapFortPlacementValidator implements GameMapValidationRule {
 
@@ -14,18 +13,16 @@ public class GameMapFortPlacementValidator implements GameMapValidationRule {
                 .filter(GameMapNode::hasAnyFort)
                 .toList();
 
+        if (!fortNodes.stream().allMatch(GameMapNode::isLootable)) {
+            note.addEntry(this, "Game map has at least one non-grass field with a fort");
+        }
+
         if (fortNodes.stream().anyMatch(GameMapNode::hasEnemyFort)) {
             note.addEntry(this, "Game map has at least one field with the enemy's fort");
         }
 
         if (fortNodes.size() != 1) {
             note.addEntry(this, "Game map has no or more than one field with a fort");
-        }
-
-        GameMapNode fortMapNode = fortNodes.getFirst();
-
-        if (fortMapNode.getTerrainType() != TerrainType.GRASS) {
-            note.addEntry(this, "Game map has a non-grass field with the fort");
         }
     }
 }