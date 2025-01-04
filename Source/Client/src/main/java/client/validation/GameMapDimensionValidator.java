09ffb7c5fe2728a873ddee6315cc21361ad4585b
Daniel Kral <a11908284@unet.univie.ac.at>
2025-01-04 12:03:33 +0100

    client: validation: improve correctness of `GameMapDimensionValidator`
    Improves correctness and readability of the game map dimension validator to
    check whether the `GameMap` contains all and only the `GameMapNode`s, which are
    expected to be in a half map's area.
    
    Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

--- Source/Client/src/main/java/client/validation/GameMapDimensionValidator.java
+++ Source/Client/src/main/java/client/validation/GameMapDimensionValidator.java
@@ -1,26 +1,25 @@
 package client.validation;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import client.map.GameMap;
+import client.map.Position;
 import client.map.PositionArea;
 
 public class GameMapDimensionValidator implements GameMapValidationRule {
 
     private static final int X_SIZE = 10;
     private static final int Y_SIZE = 5;
-    private static final int MAP_SIZE = X_SIZE * Y_SIZE;
 
     @Override
     public void validate(GameMap map, Notification<? super GameMapValidationRule> note) {
         PositionArea area = new PositionArea(0, 0, X_SIZE, Y_SIZE);
+        Set<Position> areaPositions = area.intoPositionStream()
+                .collect(Collectors.toUnmodifiableSet());
 
-        if (map.getSize() != MAP_SIZE) {
-            note.addEntry(this,
-                          String.format("Game map does contain less or more than %d fields",
-                                        MAP_SIZE));
-        }
-
-        if (map.getPositions().stream().anyMatch(area::isOutside)) {
-            note.addEntry(this, "Game Map contains fields, whose positions are out-of-bounds");
+        if (!map.getPositions().equals(areaPositions)) {
+            note.addEntry(this, "Game Map contains fields with unexpected positions");
         }
     }
 }
