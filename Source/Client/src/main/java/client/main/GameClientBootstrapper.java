b25e845392b99259a8ffca147284c4ce9bc2e114
Daniel Kral <a11908284@unet.univie.ac.at>
2025-01-05 11:47:12 +0100

    client: main: factor out pre-game bootstrapping code in builder class
    Move all code relevant to the bootstrapping of the first server-provided
    `GameClientState`, which contains the full `GameMap`, both players, and
    therefore being the actual game stage, where sending movement requests to the
    game server is possible.
    
    Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

---
+++ Source/Client/src/main/java/client/main/GameClientBootstrapper.java
@@ -0,0 +1,126 @@
+package client.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import client.generation.MapGenerator;
+import client.map.GameMap;
+import client.network.GameClientIdentifier;
+import client.network.GameClientRegistrar;
+import client.network.GameClientToken;
+import client.network.GameMapSender;
+import client.network.GameServerClient;
+import client.network.GameStateUpdater;
+import client.player.PlayerDetails;
+import client.validation.HalfMapValidator;
+
+public class GameClientBootstrapper {
+
+    private static final Logger logger = LoggerFactory.getLogger(GameClientBootstrapper.class);
+
+    private static final String FIRST_NAME = "Daniel";
+    private static final String LAST_NAME = "Kral";
+    private static final String U_ACCOUNT = "krald88";
+
+    private final String gameId;
+    private Optional<GameClientToken> clientToken;
+    private final GameServerClient serverClient;
+
+    public GameClientBootstrapper(String gameId, String serverBaseUrl) {
+        this.gameId = gameId;
+        this.clientToken = Optional.empty();
+        this.serverClient = new GameServerClient(serverBaseUrl);
+    }
+
+    private static void waitOn(String reason, Predicate<GameClientState> condition,
+                               GameStateUpdater stateUpdater, GameClientState clientState) {
+        while (!condition.test(clientState)) {
+            logger.info("{}...", reason);
+
+            clientState.update(stateUpdater.pollGameState());
+
+            GameServerClient.suspendForServer(reason.toLowerCase());
+        }
+    }
+
+    private static GameMap generateGameMap() {
+        MapGenerator mapGenerator = new MapGenerator();
+        HalfMapValidator mapValidator = new HalfMapValidator();
+
+        return mapGenerator.generateUntilValid(mapValidator);
+    }
+
+    private static GameClientToken registerPlayer(GameServerClient serverClient, String gameId) {
+        PlayerDetails playerDetails = new PlayerDetails(FIRST_NAME, LAST_NAME, U_ACCOUNT);
+        GameClientIdentifier identifier = new GameClientIdentifier(gameId, playerDetails);
+        GameClientRegistrar registrar = new GameClientRegistrar(serverClient, identifier);
+
+        return registrar.registerPlayer();
+    }
+
+    private static GameClientState sendMap(GameServerClient serverClient, GameClientToken token,
+                                           GameStateUpdater stateUpdater, GameMap gameMap) {
+        GameClientState clientState = stateUpdater.pollGameState();
+
+        waitOn("Wait for another client to join",
+               state -> state.hasBothPlayers() && state.shouldClientAct(),
+               stateUpdater, clientState);
+
+        GameMapSender mapSender = new GameMapSender(serverClient, token);
+        mapSender.sendMap(gameMap);
+
+        clientState.update(stateUpdater.pollGameState());
+
+        waitOn("Wait for the full map", GameClientState::hasFullMap, stateUpdater, clientState);
+
+        return clientState;
+    }
+
+    private GameClientToken retrieveToken() {
+        GameClientToken token = registerPlayer(serverClient, gameId);
+        logger.info("Client acquired Player ID {}", token.playerId());
+
+        this.clientToken = Optional.of(token);
+
+        return token;
+    }
+
+    /**
+     * Returns the {@link GameStateUpdater} instance produced while building the
+     * {@link GameClientState}. This method should only be called after {@link #bootstrap()}.
+     *
+     * @return the state updater of the bootstrapper
+     */
+    public GameStateUpdater getStateUpdater() {
+        // TODO: Improve error handling here
+        return new GameStateUpdater(serverClient, clientToken.orElseThrow());
+    }
+
+    /**
+     * Bootstraps and returns the {@link GameClientState}.
+     * <p>
+     * This handles the pre-game setup and requests to the server to provide the
+     * {@link GameClientState}, which is ready to engage in the actual game, where the full
+     * {@link GameMap} and both players are present, as well as sending map movement requests
+     * is possible.
+     *
+     * @return the server-provided game-ready game state
+     */
+    public GameClientState bootstrap() {
+        GameMap gameMap = generateGameMap();
+        logger.info("Client generated the following player's half map\n{}", gameMap);
+
+        GameClientToken token = retrieveToken();
+        GameStateUpdater stateUpdater = getStateUpdater();
+        GameClientState clientState = sendMap(serverClient, token, stateUpdater, gameMap);
+
+        GameMap currentMap = clientState.getMap();
+        logger.info("Client received the full map\n{}", currentMap);
+
+        return clientState;
+    }
+
+}
--- Source/Client/src/main/java/client/main/MainClient.java
+++ Source/Client/src/main/java/client/main/MainClient.java
@@ -13,56 +13,19 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-import client.generation.MapGenerator;
 import client.map.GameMap;
 import client.map.GameMapNode;
 import client.map.MapDirection;
 import client.map.Position;
-import client.network.GameClientIdentifier;
-import client.network.GameClientRegistrar;
-import client.network.GameClientToken;
-import client.network.GameMapSender;
 import client.network.GameServerClient;
 import client.network.GameStateUpdater;
-import client.player.PlayerDetails;
 import client.search.AStarPathFinder;
 import client.search.PathFinder;
 import client.util.ANSIColor;
-import client.validation.HalfMapValidator;
 
 public class MainClient {
 
     private static final Logger logger = LoggerFactory.getLogger(MainClient.class);
-
-    private static final String FIRST_NAME = "Daniel";
-    private static final String LAST_NAME = "Kral";
-    private static final String UACCOUNT = "krald88";
-
-    private static GameMap generateGameMap() {
-        MapGenerator mapGenerator = new MapGenerator();
-        HalfMapValidator mapValidator = new HalfMapValidator();
-
-        return mapGenerator.generateUntilValid(mapValidator);
-    }
-
-    private static GameClientToken registerPlayer(GameServerClient serverClient, String gameId) {
-        PlayerDetails playerDetails = new PlayerDetails(FIRST_NAME, LAST_NAME, UACCOUNT);
-        GameClientIdentifier identifier = new GameClientIdentifier(gameId, playerDetails);
-        GameClientRegistrar registrar = new GameClientRegistrar(serverClient, identifier);
-
-        return registrar.registerPlayer();
-    }
-
-    private static void waitOn(String reason, Predicate<GameClientState> condition,
-                               GameStateUpdater stateUpdater, GameClientState clientState) {
-        while (!condition.test(clientState)) {
-            logger.info("{}...", reason);
-
-            clientState.update(stateUpdater.pollGameState());
-
-            GameServerClient.suspendForServer(reason.toLowerCase());
-        }
-    }
 
     private static void runStage(String reason, Predicate<GameClientState> condition,
                                  GameStateUpdater stateUpdater, GameClientState clientState,
@@ -86,24 +49,6 @@ public class MainClient {
 
             GameServerClient.suspendForServer(reason.toLowerCase());
         }
-    }
-
-    private static GameClientState sendMap(GameServerClient serverClient, GameClientToken token,
-                                           GameStateUpdater stateUpdater, GameMap gameMap) {
-        GameClientState clientState = stateUpdater.pollGameState();
-
-        waitOn("Wait for another client to join",
-               state -> state.hasBothPlayers() && state.shouldClientAct(),
-               stateUpdater, clientState);
-
-        GameMapSender mapSender = new GameMapSender(serverClient, token);
-        mapSender.sendMap(gameMap);
-
-        clientState.update(stateUpdater.pollGameState());
-
-        waitOn("Wait for the full map", GameClientState::hasFullMap, stateUpdater, clientState);
-
-        return clientState;
     }
 
     private static List<GameMapNode> getUnvisitedMapNodes(Collection<GameMapNode> mapNodes) {
@@ -256,19 +201,9 @@ public class MainClient {
         String serverBaseUrl = args[1];
         String gameId = args[2];
 
-        GameMap gameMap = generateGameMap();
-        logger.info("Client generated the following player's half map\n{}", gameMap);
-
-        GameServerClient serverClient = new GameServerClient(serverBaseUrl);
-
-        GameClientToken token = registerPlayer(serverClient, gameId);
-        logger.info("Client acquired Player ID {}", token.playerId());
-
-        GameStateUpdater stateUpdater = new GameStateUpdater(serverClient, token);
-        GameClientState clientState = sendMap(serverClient, token, stateUpdater, gameMap);
-
-        GameMap currentMap = clientState.getMap();
-        logger.info("Client received the full map\n{}", currentMap);
+        GameClientBootstrapper bootstrapper = new GameClientBootstrapper(gameId, serverBaseUrl);
+        GameClientState clientState = bootstrapper.bootstrap();
+        GameStateUpdater stateUpdater = bootstrapper.getStateUpdater();
 
         runStage("finding the player's treasure",
                  GameClientState::hasFoundTreasure,
