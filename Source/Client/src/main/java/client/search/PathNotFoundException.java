e856dd67d38274b486cdd133d1195cbd6142e0cd
Daniel Kral <a11908284@unet.univie.ac.at>
2025-01-05 17:11:50 +0100

    client: search: remove unused single arg-constructor in `PathNotFoundException`
    Signed-off-by: Daniel Kral <a11908284@unet.univie.ac.at>

--- Source/Client/src/main/java/client/search/PathNotFoundException.java
+++ Source/Client/src/main/java/client/search/PathNotFoundException.java
@@ -6,10 +6,6 @@ public class PathNotFoundException extends RuntimeException {
 
     private static final String ERROR_MESSAGE_TEMPLATE = "Could not find any path from %s to %s";
 
-    public PathNotFoundException(String message) {
-        super(message);
-    }
-
     public PathNotFoundException(Position source, Position destination) {
         super(String.format(ERROR_MESSAGE_TEMPLATE, source, destination));
     }
